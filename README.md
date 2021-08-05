186ae3ac66dabec0ccf8dc466a7a274e0dd5de66
cristian camilo gutierrez correa <gccc1108@hotmail.com>
2021-08-05 16:54:27 -0500

    improvement

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Guess-my-Number-game
 
 ## Some Dom manipulation and html classes
+
+```sh
+cursor: pointer; ---->
+```
+
+The cursor CSS property sets the type of mouse cursor, if any, to show when the mouse pointer is over an element.
+
+useful when we want to change the way the pinter looks when hovering over a button or an image.
+
+e.g
+
+```sh
+cursor: not-allowed
+- it displays an stop sign
+cursor: grab
+-It displays a hand
+```
--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const displayMessage = function (message) {
 document.querySelector(`.check`).addEventListener(`click`, function () {
   let guess = Number(document.querySelector(`.guess`).value);
   console.log(guess);
+  document.querySelector('.guess').value = ``;
+  document.querySelector('.guess').focus();
 
   if (!guess) {
     displayMessage(`No number!`);
@@ -19,6 +21,8 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
     document.querySelector(`.number`).textContent = secretNumber;
     document.querySelector(`body`).style.backgroundColor = `#60b347`;
     document.querySelector(`.number`).style.width = `30rem `;
+
+    document.querySelector(`.check`).hidden = true;
 
     if (score > highscore) {
       highscore = score;
@@ -35,6 +39,8 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
 });
 
 document.querySelector(`.again`).addEventListener(`click`, function () {
+  document.querySelector(`.check`).hidden = false;
+  document.querySelector(`.guess`).focus();
   document.querySelector(`body`).style.backgroundColor = `#222`;
   document.querySelector(`.number`).textContent = `?`;
   document.querySelector(`.number`).style.width = `15rem`;
